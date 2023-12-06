2f7574881108ef33ff7b4c1144ff647bac08ea7a
David Nguyen <124234643+Pzychopomp@users.noreply.github.com>
2023-12-05 16:03:03 -0800

    Add files via upload

---
+++ run-game.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Bash script for Coinflip Tic-Tac-Toe
+echo "Starting up Random turn Tic-Tac-Toe . . ."
+
+# Assuming 'python' refers to Python 3 on your system
+python3 main.py
+
+read -p "Press Enter to continue..."
+
+exit 0
